5ded7747c4d8255b7f55e193570471630cbe6526
mostafamahmood17 <taat.heritage@gmail.com>
2020-11-16 22:16:28 -0800

    google and email done

--- src/App.js
+++ src/App.js
@@ -6,30 +6,42 @@ import {
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
+import Login from './components/Login/Login';
+import { createContext, useState } from 'react';
+export const UserContext = createContext();
 
 
 
 
 function App() {
-  
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    
-      <Router>
 
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
 
-      </Router>
-    
+    <Router>
+
+      <Switch>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+
+      </Switch>
+
+
+    </Router>
+   </ UserContext.Provider>
+
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAJjtTLGY2hn95gRiY4UNY_V_fnzKZCl_g",
+    authDomain: "apartment-pro.firebaseapp.com",
+    databaseURL: "https://apartment-pro.firebaseio.com",
+    projectId: "apartment-pro",
+    storageBucket: "apartment-pro.appspot.com",
+    messagingSenderId: "964989770527",
+    appId: "1:964989770527:web:88443de1e71975b7472e2a"
+  };
+
+  export default firebaseConfig;
+
